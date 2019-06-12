50f7b4897032a529e13f442588343fb6813ba8bd
Leon LL Zhang <leon.ll.zhang@cn.pwc.com>
2019-06-12 18:46:53 +0800

    add moment

--- src/main.js
+++ src/main.js
@@ -2,8 +2,10 @@ import Vue from 'vue'
 import './plugins/vuetify'
 import App from './App.vue'
 
-Vue.config.productionTip = false
 
-new Vue({
+Vue.config.productionTip = false
+Vue.config.devtools = true;
+
+new Vue({  
   render: h => h(App),
 }).$mount('#app')
